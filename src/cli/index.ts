12c79c183a3854d7ae8c83834606e0fc8a40d035
artur <artur.ayup@gmail.com>
2024-05-01 16:38:43 +0400

    Создает cli приложение

---
+++ src/cli/index.ts
@@ -0,0 +1,5 @@
+export { CLIApplication } from './cli-application.js';
+export { CommandParser } from './command-parser.js';
+
+export { VersionCommand } from './commands/version.command.js';
+export { HelpCommand } from './commands/help.command.js';
