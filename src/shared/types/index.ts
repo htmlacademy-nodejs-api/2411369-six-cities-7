5c9e6155892edde0cb4632d84888335418ea7ca6
artur <artur.ayup@gmail.com>
2024-05-09 20:27:46 +0400

    Реализует в приложении логирование

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,3 +5,4 @@ export { Offer } from './offer.type.js';
 export { User } from './user.type.js';
 export { MockServerData } from './mock-server-data.type.js';
 export { Location } from './location.type.js';
+export { Component } from './component.enum.js';
